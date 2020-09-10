d3320b02c15feebdbb778cad9116b9e09a939d2e
Bianca_Reutergårdh <biareu0303@gmail.com>
2020-09-10 14:48:03 +0200

    Initial Commit

---
+++ någott/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace någott
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                string answerone;
+
+
+                Console.WriteLine("HEEEEJJJJJJ DAMER OCH HERRAR, VÄLKOMMNA TILL 'EN FRÅGESPORT'");
+                Console.WriteLine("TODAY SKA JAG FRÅGA DIG FRÅGA.1 vilket lyder..: Du ser en haj simma i oceanens djup. Hur agerar du?");
+                Console.WriteLine("1 SLÅ NER HONOM, 2 SIMMA LÅNGSAMT BAKÅT, 3 SIMMA ZIG ZAG BROR");
+                answerone = Console.ReadLine();
+                if (answerone == "1")
+                {
+                    Console.WriteLine("Detta svar var rätt nice bror");
+
+                    svårare();
+                }
+                else if (answerone == "2")
+                {
+                    Console.WriteLine("smart men lite whack bror");
+                }
+                else
+                {
+                    Console.WriteLine("fucking retard bror");
+
+
+                }
+
+
+
+                static void svårare();
+
+                Console.WriteLine("Du är uppenbarligen en hög intelligent människa och därför ska du nu få svårare frågor. >:)");
+                Console.WriteLine("FRÅGA2!!! Vad ");
+
+                Console.ReadLine();
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //            Console.WriteLine("ditt name bih");
+            //            string name = Console.ReadLine();
+            //
+            //            if (name == "bibbi" || name == "sofie")
+            //            {
+            //                Console.WriteLine("hejhej" + name);
+            //            }
+            //            else if (name == "bertil")
+            //            {
+            //                Console.WriteLine("omg bertil hello");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("dumb hoe, get outta heeer");
+            //            }
+
+            //            if (name != "bibbi")
+            //            {
+            //                Console.WriteLine("gitt bram");
+            //            }
+
+            //           Console.ReadLine();
+        }
+    }
+}
